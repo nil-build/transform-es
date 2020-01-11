cce4e3df648bb94d70034c651310b4e8610c1695
bplok20010 <nobo.zhou@foxmail.com>
2020-01-11 21:20:12 +0800

    update

--- test/src/typescript/evaluate.ts
+++
@@ -1,6 +0,0 @@
-import { VMContext } from "./types";
-import { runInContext } from "./vm";
-
-export default (code: string, ctx?: VMContext) => {
-	return runInContext(code, ctx);
-};
--- test/src/typescript/index.ts
+++
@@ -1,6 +0,0 @@
-import { Interpreter } from "./interpreter/index";
-import * as vm from "./vm";
-import evaluate from "./evaluate";
-import Function from "./Function";
-
-export { Interpreter, vm, evaluate, Function };
